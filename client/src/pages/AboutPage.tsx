632ca59eb9af1c7e52f5896bfed1944c06a52b96
Wonyoung Jung <2655362@naver.com>
2026-02-09 00:14:15 +0900

    Modify error of AboutPage

--- client/src/pages/AboutPage.tsx
+++ client/src/pages/AboutPage.tsx
@@ -128,7 +128,7 @@ const AboutPage = () => {
           content: res.data.content || '',
           quote: res.data.quote || '',
           // 이미지가 없으면 기본 로고 사용
-          image: res.data.imageUrl ? `$${res.data.imageUrl}` : assets.logo_uic,
+          image: res.data.imageUrl ? res.data.imageUrl : assets.logo_uic,
         });
 
         setExecutives([
